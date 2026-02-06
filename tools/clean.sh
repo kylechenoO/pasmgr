e7036750560841ba17a223b38cf97209a03238b8
Kyle Chen <kyle@hacking-linux.com>
2026-02-06 16:23:39 +0800

    fix db init issue

--- tools/clean.sh
+++ tools/clean.sh
@@ -2,6 +2,6 @@
 
 declare -x PROJ_PATH=$(dirname $(dirname $(realpath $0)))
 rm -rvf $(find ${PROJ_PATH} -name '__pycache__' | xargs)
-rm -rvf ${PROJ_PATH}/bin/{Activate.ps1,activate,activate.csh,activate.fish,pip,pip3,pip3.12,python,python3,python3.12}
+rm -rvf ${PROJ_PATH}/bin/{Activate.ps1,activate,activate.csh,activate.fish,pip,pip3,pip3.12,python,python3,python3.12,alembic,dotenv,fastapi,mako-render,uvicorn}
 rm -rvf ${PROJ_PATH}/{include,lib,lib64,pyvenv.cfg}
 
